f68de1c3f04b7f6bc7d8588c5dbd08467625441d
Anthony Spedaliere <avspedaliere@gmail.com>
2025-05-12 12:45:33 -0700

    created deployment logic using hardhat ignition

---
+++ hardhat-project/ignition/modules/verifier.js
@@ -0,0 +1,16 @@
+/**
+ * @title Verifier Ignition Module
+ * @author Anthony Spedaliere
+ * @notice Ignition module for deploying verifier.sol
+ * @dev This module deploys the verifier contract that validates zk-SNARK proofs
+ */
+
+const { buildModule } = require("@nomicfoundation/hardhat-ignition-ethers");
+
+module.exports = buildModule("VerifierModule", (m) => {
+  const verifier = m.contract("Groth16Verifier");
+
+  return {
+    verifier,
+  };
+});
